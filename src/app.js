7c39db7bdc6c137656271ada17feddc264c18e3d
urvesh <ukchauhan0347@gmail.com>
2026-02-12 12:02:25 +0530

    utils code apiErrorHandle  apiResponse

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true,
+}))
+
+app.use(express.json());//middleware accept json data
+app.use(express.urlencoded({extended : true}));// accept data from url
+app.use(express.static("public"));//store static file in public
+app.use(cookieParser());//
+
+export {app}
