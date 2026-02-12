8fa43d31e77e3adeaa52baf0a951001826933598
urvesh <ukchauhan0347@gmail.com>
2026-02-12 20:19:08 +0530

    update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,7 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const registerUser = asyncHandler( async(req , res)=>{
-
-    res.send("working correct!!!");
-    res.status(200).json({
+    return  res.status(200).json({
         message:'ok'
     })
 });
